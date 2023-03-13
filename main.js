b6c6f491ae92eadd3e483ce17081447d6aed4f0e
StuwarNewmann <contact.stuwar@gmail.com>
2023-03-13 02:02:58 -0500

    list of nodes, realtime changes

--- main.js
+++ main.js
@@ -1,14 +1,40 @@
 
 //Importe de funcion personalizada modularizada.
-import { saveNode } from "./data/firebase.js"; 
+import { saveNode, onGetNodes} from "./data/firebase.js"; 
 
-//Funcion que permite que cada vez que se recargue la pagina se recargue el contenido del DOM.
-window.addEventListener('DOMContentLoaded', () => {
-    console.log('Works');
-});
+
+
 
 //Seleccion de Formulario y guardado de estado en una constante.
 const nodeForm = document.getElementById('node-form');
+//Seleccion de nodos y guardado de estado en una constante
+const nodeContainer = document.getElementById('ndoe-container');
+
+
+//Funcion que permite que cada vez que se recargue la pagina se recargue el contenido del DOM.
+window.addEventListener('DOMContentLoaded',  async () => {
+    //Esta linea es un querySnapshot(onSnapshot) que nos trae los datos que existen en ese momento,  es una funcion asincrona pues es una base de datos en tiempo real 
+    
+    onGetNodes((nodes) => {
+        let html = '';
+        //Recorremos los datos
+        nodes.forEach(doc => {
+            //Doc nos trae segmennada la propiedad(data)
+            //doc.data transforma el resultado en un objeto de js y no uno de firebase
+            const node = doc.data();
+            html += `
+            <div>
+                <h3>${node.node}</h3>
+                <p>${node.description}</p>
+            </div>
+            `
+        });
+        nodeContainer.innerHTML = html;
+    });
+
+
+});
+
 
 //Escucha del evento(Submit) y funcion asincrona que llama a fireStore para guardar datos.
 nodeForm.addEventListener('submit', (event) => {
